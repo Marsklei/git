7b93226055453b27e0b006900a971e6e4a486c42
lilei <lilei@126.com>
2019-01-03 19:45:20 +0800

    添加测试

---
+++ git/src/test.java
@@ -0,0 +1,16 @@
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Autor 李孟雷
+ * @Date 2019/1/3 19:41
+ * @Version 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("执行了执行了");
+        System.out.println("执行了执行了");
+        System.out.println("执行了执行了");
+        System.out.println("执行了执行了");
+        System.out.println("执行了执行了");
+    }
+}
